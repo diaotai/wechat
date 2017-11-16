7c37d8b69c9184e663cc04a874b912eab617ad4a
diaotai <diaotai123@gmail.com>
2017-11-16 10:39:24 +0800

    ax

--- app.js
+++ app.js
@@ -19,16 +19,15 @@ app.use(express.query());
 
 app.use('/', wechat(config, function (req, res, next) {
     var message = req.weixin;
-    // let axios = require("axios");
+    let axios = require("axios");
     
-    // axios.get(`http://www.tuling123.com/openapi/api?key=262202cd50b04864bb12238210d9845b&info=${message.}&userid=3245232`)
-    //   .then(function (response) {
-    //     console.log(response.data.text);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    // });
-      //你也可以这样回复text类型的信息 
+    axios.get(`http://www.tuling123.com/openapi/api?key=262202cd50b04864bb12238210d9845b&info=${message.content}&userid=${message.FromUserName}`)
+      .then(function (response) {
+        console.log(response.data.text);
+      })
+      .catch(function (error) {
+        console.log(error);
+    }); 
     
     console.log("接收消息",message);
     res.reply({
