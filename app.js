cd88126e996a205e844fdbd8ded14654c3abcffa
diaotai <diaotai123@gmail.com>
2017-11-16 10:50:20 +0800

    自动回复

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 // 引用 express 来支持 HTTP Server 的实现
-const express = require('express');
+const express = require("express");
 
 // 引用微信公共平台自动回复消息接口服务中间件
-var wechat = require('wechat');
+var wechat = require("wechat");
 
 // 创建一个 express 实例
 const app = express();
 
 // 配置微信公众平台参数，在教程第二步中获取
 var config = {
-    token: 'token', // 填第二步中获取的 `token`
-    appid: 'wxe481882d4f9c57c1', // 填第二步中获取的 `appid`
-    encodingAESKey: '3iJyVWWeinFbWCFxgqGTQolQGwX9WDdTqZxUN0SWLLR', // 填第二步中获取的 `encodingAESKey`
-    checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false 
+  token: "token", // 填第二步中获取的 `token`
+  appid: "wxe481882d4f9c57c1", // 填第二步中获取的 `appid`
+  encodingAESKey: "3iJyVWWeinFbWCFxgqGTQolQGwX9WDdTqZxUN0SWLLR", // 填第二步中获取的 `encodingAESKey`
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
 
 app.use(express.query());
 
-app.use('/', wechat(config, function (req, res, next) {
+app.use(
+  "/",
+  wechat(config, function(req, res, next) {
     var message = req.weixin;
     let axios = require("axios");
-    
-    axios.get(`http://www.tuling123.com/openapi/api?key=262202cd50b04864bb12238210d9845b&info=${message.Content}&userid=${message.FromUserName}`)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-    }); 
-    
-    console.log("接收消息",message);
-    res.reply({
-        content: '你好，Hello World!',
-        type: 'text'
-    });
-}));
 
-console.log("hello")
+    axios
+      .get(
+        `http://www.tuling123.com/openapi/api?key=262202cd50b04864bb12238210d9845b&info=${message.Content}&userid=${message.FromUserName}`
+      )
+      .then(function(response) {
+        res.reply({
+          content: response.data.text,
+          type: "text"
+        });
+        //console.log();
+      })
+      .catch(function(error) {
+        res.reply({
+          content: "出现错误",
+          type: "text"
+        });
+      });
+  })
+);
+
+console.log("hello");
 
 // 监听端口，等待连接
 const port = 5050;
